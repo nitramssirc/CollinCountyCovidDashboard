593b470da9b5c059aeb35fb2322ecb15869c9a40
Criss Martin <nitramssirc@yahoo.com>
2020-12-20 20:11:16 -0600

    Refactored New Case Trend page so the Trend Chart can be reused

---
+++ CollinCountyCovidDashboard/Client/Models/TrendChartRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollinCountyCovidDashboard.Client.Models
+{
+    public class TrendChartRecord<T>
+    {
+        public DateTime Date { get; private set; }
+
+        public T DailyValue { get; private set; }
+
+        public T SevenDayAvg { get; private set; }
+
+        public TrendChartRecord(DateTime date, T dailyValue, T sevenDayAvg)
+        {
+            Date = date;
+            DailyValue = dailyValue;
+            SevenDayAvg = sevenDayAvg;
+        }
+
+    }
+}
--- CollinCountyCovidDashboard/Client/Pages/NewCaseTrends.razor.cs
+++ CollinCountyCovidDashboard/Client/Pages/NewCaseTrends.razor.cs
@@ -1,6 +1,10 @@
-﻿using Application.Queries.GetNewCases;
+﻿using Application.BaseModels;
+using Application.Queries.GetNewCases;
 
 using Blazorise.Charts;
+
+using CollinCountyCovidDashboard.Client.Models;
+using CollinCountyCovidDashboard.Client.Shared;
 
 using Microsoft.AspNetCore.Components;
 
@@ -13,103 +17,28 @@ namespace CollinCountyCovidDashboard.Client.Pages
 {
     public partial class NewCaseTrends
     {
-        LineChart<int> lineChart;
-
-        object chartOptions = new
-        {
-            scales = new
-            {
-                xAxes = new object[]
-                {
-                    new
-                    {
-                        ticks=new
-                        {
-                            fontColor = "#fff"
-                        }
-                    }
-                },
-                yAxes = new object[]
-                {
-                    new
-                    {
-                        ticks=new
-                        {
-                            fontColor = "#fff"
-                        }
-                    }
-                },
-            },
-            legend = new
-            {
-                labels = new
-                {
-                    fontColor="#fff"
-                }
-            },
-            responsive = true,
-            maintainAspectRatio = false,
-        };
+        TrendChart<int> _trendChart;
 
         [Inject] IGetNewCasesQuery _getNewCasesQuery { get; set; }
+
+
+
+        #region Overrides
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
-                await HandleRedraw();
+                var queryResults = await _getNewCasesQuery.Execute(30);
+                await _trendChart.SetChartData(ConstructTrendChartRecords(queryResults));
             }
         }
 
-        async Task HandleRedraw()
-        {
-            await lineChart.Clear();
-            var newCasesData = await _getNewCasesQuery.Execute(30);
-            await lineChart.AddLabelsDatasetsAndUpdate(
-                GetDateLabels(newCasesData.Result),
-                Get7DayAverageDataSet(newCasesData.Result),
-                GetNewCaseCountDataSet(newCasesData.Result));
-        }
+        #endregion
 
-        string[] GetDateLabels(NewCaseModel[] result)
+        private TrendChartRecord<int>[] ConstructTrendChartRecords(QueryResult<NewCaseModel[]> queryResults)
         {
-            return result.Select(d => d.Date.ToShortDateString()).ToArray();
+            return queryResults.Result.Select(r => new TrendChartRecord<int>(r.Date, r.NewCases, (int)Math.Round(r.SevenDayAvg))).ToArray();
         }
-
-        LineChartDataset<int> GetNewCaseCountDataSet(NewCaseModel[] result)
-        {
-            return new LineChartDataset<int>
-            {                
-                Data = result.Select(r=>r.NewCases).ToList(),
-                BackgroundColor = new List<string> { ChartColor.FromRgba(255, 99, 132, 0.2f) },
-                BorderColor = new List<string> { ChartColor.FromRgba(255, 99, 132, 1f) },
-                Fill = true,
-                SteppedLine = true,
-                Label = "New Confirmed Cases",
-                PointRadius = 4,
-                PointBorderColor = Enumerable.Repeat("#ff6384", result.Length).ToList(),
-                PointBackgroundColor = Enumerable.Repeat("#ff6384", result.Length).ToList()
-            };
-        }
-
-        LineChartDataset<int> Get7DayAverageDataSet(NewCaseModel[] result)
-        {
-            return new LineChartDataset<int>
-            {
-                Data = result.Select(r => (int)Math.Round(r.SevenDayAvg)).ToList(),
-                BorderColor = new List<string> { ChartColor.FromRgba(255, 255, 255, 1f) },
-                Fill = false,
-                PointRadius = 4,
-                BorderDash = new List<int> { },
-                SteppedLine = false,
-                Label = "7 Day Average",
-                PointBorderColor = Enumerable.Repeat("#fff", result.Length).ToList(),
-                PointBackgroundColor = Enumerable.Repeat("#fff", result.Length).ToList()
-            };
-        }
-
-        List<string> backgroundColors = new List<string> { ChartColor.FromRgba(255, 99, 132, 0.2f) };
-        List<string> borderColors = new List<string> { ChartColor.FromRgba(255, 99, 132, 1f) };
-        private object r;
     }
 }
