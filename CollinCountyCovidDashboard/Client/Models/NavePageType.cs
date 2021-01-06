b8fcd698805e19d0bb66c229dbfcf778ff3346e0
Criss Martin <nitramssirc@yahoo.com>
2021-01-05 21:53:35 -0600

    Created Quick Nav Menu

---
+++ CollinCountyCovidDashboard/Client/Models/NavePageType.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+public enum NavPageType
+{
+    Latest,
+    SevenDayAvg,
+    NewCaseTrends,
+    NewDeathsTrend,
+    PositivityRateTrend,
+    HospitalBedsTrend,
+    ICUBedsTrend,
+    Sources
+}
+
+public static class NavePageTypeExt
+{
+    public static string Description(this NavPageType navPageType)
+    {
+        return navPageType switch
+        {
+            NavPageType.SevenDayAvg => "7 Day Averages",
+            NavPageType.NewCaseTrends => "New Cases Trend",
+            NavPageType.NewDeathsTrend => "New Deaths Trend",
+            NavPageType.PositivityRateTrend => "Positivity Rate Trend",
+            NavPageType.HospitalBedsTrend => "Hospital Beds Trend",
+            NavPageType.ICUBedsTrend => "ICU Beds Trend",
+            _ => navPageType.ToString(),
+        };
+    }
+}
---
+++ CollinCountyCovidDashboard/Client/Shared/QuickNav.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollinCountyCovidDashboard.Client.Shared
+{
+    public partial class QuickNav
+    {
+        #region Properties
+
+        private string MenuAnimationClass { get; set; }
+
+        #endregion
+
+        #region Event Handler
+
+        private void MenuButtonClicked(MouseEventArgs e)
+        {
+            ShowMenu();
+        }
+
+        private void ClickAwayRegionClicked(MouseEventArgs e)
+        {
+            HideMenu();
+        }
+
+        private void NavLinkClicked(MouseEventArgs e)
+        {
+            HideMenu();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ShowMenu()
+        {
+            MenuAnimationClass = "slideOut";
+        }
+
+        private void HideMenu()
+        {
+            MenuAnimationClass = "slideIn";
+        }
+
+        #endregion
+    }
+}
