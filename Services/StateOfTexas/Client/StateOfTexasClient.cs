dd8b277250500f78b93cf2720cc658afdb136124
Criss Martin <nitramssirc@yahoo.com>
2021-01-15 17:05:31 -0600

    Data Update 1/15/2021

--- Services/StateOfTexas/Client/StateOfTexasClient.cs
+++ Services/StateOfTexas/Client/StateOfTexasClient.cs
@@ -397,9 +397,9 @@ namespace Services.StateOfTexas.Client
 
             var splitDate = dateString.Split("-");
             var hasYear = splitDate.Length == 3;
-            var month = int.Parse(splitDate[hasYear ? 1 : 0]);
-            var day = int.Parse(splitDate[hasYear ? 2 : 1]);
-            var year = hasYear ? int.Parse(splitDate[0]) : month < 3 ? 2021 : 2020;
+            var month = int.Parse(splitDate[0]);
+            var day = int.Parse(splitDate[1]);
+            var year = hasYear ? int.Parse(splitDate[2]) : month < 3 ? 2021 : 2020;
 
             return new DateTime(year, month, day);
         }
